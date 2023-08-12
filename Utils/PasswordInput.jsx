ea17265bb479f88fc86c14308fa83d19de9f6b0d
mr-ridhor <surajudeen.ridwan.a@gmail.com>
2023-08-12 23:30:51 +0100

    login

---
+++ Utils/PasswordInput.jsx
@@ -0,0 +1,39 @@
+'use client'
+import React,{useState} from 'react'
+import {AiOutlineEye,AiOutlineEyeInvisible} from 'react-icons/ai'
+const PasswordInput = ({onChange,onchange,value,label}) => {
+    const [type, setType] = useState("password");
+ 
+    return (
+    <div className="w-full flex flex-row items-center pr-2 border-2 border-neutral-300 bg-white rounded-md">
+    <div className="w-full relative ">
+      <input
+    
+        placeholder=" "
+        onChange={onchange ?? onChange}
+        type={type}
+        value={value}
+        className="peer w-full pt-4 mt-1  outline-none transition disabled:opacity-70 disabled:cursor-not-allowed pl-2 focus:outline-none text-gray-800"
+      />
+      <label
+       
+        className="absolute cursor-text text-sm duration-150 transform -translate-y-3 top-3 z-10 origin-[0] left-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 text-zinc-400 font-semibold"
+      >
+        {label}
+      </label>
+    </div>
+    <div
+      className="cursor-pointer"
+      onClick={() => setType(type == "password" ? "text" : "password")}
+    >
+      {type == "password" ? (
+        <AiOutlineEye className="w-6 h-6 text-gray-400" />
+      ) : (
+        <AiOutlineEyeInvisible className="w-6 h-6 text-gray-400" />
+      )}
+    </div>
+  </div>
+  )
+}
+
+export default PasswordInput
---
+++ components/LoginForm.jsx
@@ -0,0 +1,20 @@
+'use client'
+import React, { useState } from 'react'
+import Input from '@/Utils/Input'
+import Head from 'next/head'
+import PasswordInput from '@/Utils/PasswordInput'
+export const LoginForm = () => {
+    const [email, setEmail]=useState("")
+  return (
+    <div className=' h-screen w-screen grid place-items-center '>
+        <div className="h-1/2 w-[350px]  bg-white shadow-lg rounded-lg">
+            <div className="my-2 mx-auto w-[95%]">
+            <Input type={"email"} value={""} Onchange={""} placeholder={"Please enter your mail"} label={"Email"}/>
+            </div>
+            <div className="my-2 mx-auto w-[95%]">
+                <PasswordInput  label={"Password"}/>
+            </div>
+        </div>
+    </div>
+  )
+}
